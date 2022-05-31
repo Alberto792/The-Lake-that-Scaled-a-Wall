365b5ab8553a12627b2210f00aafeefe01b90fd5
Alberto Paz <alberto_paz@uri.edu>
2022-05-31 11:26:52 -0400

    notes on functions

---
+++ future ref.R
@@ -0,0 +1,11 @@
+abt <- function(input1, input2){
+	cat("Hello", input1, input2, sep="\t")
+	functionResult <- input1 *input2
+	return(functionResult)
+}
+
+input1 <- "ghslfjdaslk"
+
+var1 <- abt(input1 = 5, input2 = 78)
+
+
