f863eae3f920ead64b490c0e6045d3f1c96ba989
Alberto Paz <alberto_paz@uri.edu>
2022-06-17 15:13:33 -0400

    Updated form

--- Game_Show_Success?.R
+++ Game_Show_Success?.R
@@ -6,12 +6,21 @@ megatron<-1000
 for (Alberto_door in 1:megatron) {
   chosen_door <- sample(c("Goat_13","Goat_007","Ford_150"),1)
   if(chosen_door=="Ford_150") 
-    Switching_Or_Not<-sample(c("Goat_007","Ford_150"),1)
+    #remove one door by sampling doors
+    Switching_Doors<-sample(c("Goat_007","Goat_13"),1)
+  if(Switching_Doors=="Goat_007") Lets_Switch<-"Goat_13"
+  if(Switching_Doors=="Goat_13") Lets_Switch<-"Goat_007"
+
   if(chosen_door=="Goat_007")
-    Switching_Or_Not<-sample(c("Goat_007","Ford_150"),1)
+    #remove Goat_13 from the door options
+    #switching from goat
+    Lets_Switch<-"Ford_150"
+  
   if(chosen_door=="Goat_13")
-    Switching_Or_Not<-sample(c("Goat_13","Ford_150"),1)
-  door_after_option_to_switch <- Switching_Or_Not
+    #remove Goat_007 from the door options
+    #switching from goat
+    Lets_Switch<-"Ford_150"
+  door_after_option_to_switch <- Lets_Switch
   if(door_after_option_to_switch=="Ford_150"){Wins<- Wins+1
   } else {Lose<-Lose+1}
   print(door_after_option_to_switch)
